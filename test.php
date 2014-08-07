fd3e09513a8c0e3911580cad127698738915b74b
chintanabsas <chintan@absas.net>
2014-08-07 12:17:42 +0530

    add on more function in js file.

--- test.php
+++ test.php
@@ -4,6 +4,7 @@
 <script>
 $(document).ready(function() {
 
+alert("dchgfhjfjgjuhuk");
 window.parent.document.getElementById('#target'); 
  var select_frame = document.getElementById("select_frame");
             var select_framedocument=select_frame.contentDocument||select_frame.contentWindow.document;
@@ -12,6 +13,18 @@ window.parent.document.getElementById('#target');
 
 
 });
+
+function test()
+{
+
+window.parent.document.getElementById('#target'); 
+ var select_frame = document.getElementById("select_frame");
+            var select_framedocument=select_frame.contentDocument||select_frame.contentWindow.document;
+            var inputIframe = select_framedocument.getElementById("select_name");
+            alert(inputIframe.value);
+
+
+}
 </script>
 
 <iframe name='select_frame' id ="select_frame" src='select.php?initial_name=jim'></iframe>
